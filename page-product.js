836fa98f8db89b09914aa4fe004e5ffd01fea01b
MaddChadd <trtoph@gmail.com>
2022-11-15 22:43:29 +0300

    реализация сохранение неотправленной формы (вер.2)

--- page-product.js
+++ page-product.js
@@ -19,15 +19,19 @@ function handleSubmit (event) {
     let rating = inputRating.value;
 
     let errorName = "";
+    
+    let isNameError = name.length === 0;
+    let isNameError1 = name.length < 2;
+
             
-    if (name.length === 0) {
+    if (isNameError) {
         errorName = "Вы забыли указать имя и фамилию";
-        inputName.style.borderColor = "#DA4A0C";
-    } else if (name.length < 2) {
+        inputName.style.borderColor = "#DA4A0C";        
+    } else if (isNameError1) {
         errorName = "Имя не может быть короче 2-х символов";
-        inputName.style.borderColor = "#DA4A0C";
+        inputName.style.borderColor = "#DA4A0C";        
     } else {
-        inputName.style.borderColor = "#888888";
+        inputName.style.borderColor = "#888888";        
     }
 
     errorElemName.innerText = errorName;
@@ -36,7 +40,9 @@ function handleSubmit (event) {
 
     let errorRating = "";
 
-    if (rating < 1 || rating > 5) {
+    let isRatingError = rating < 1 || rating > 5;
+
+    if (isRatingError) {
         errorRating = "Оценка должна быть от 1 до 5";
         inputRating.style.borderColor = "#DA4A0C";
     } else {
@@ -46,10 +52,12 @@ function handleSubmit (event) {
     errorElemRating.innerText = errorRating;
     errorElemRating.classList.toggle("visible", errorRating);
 
-   localStorage.removeItem("name");
-   localStorage.removeItem("rating");
-   localStorage.removeItem("text");   
-   
+    if (!isNameError && !isNameError1 && !isRatingError) {
+        localStorage.removeItem("name");
+        localStorage.removeItem("rating");
+        localStorage.removeItem("text"); 
+    }
+
    console.log("Sumbit");
 }
 
@@ -69,3 +77,55 @@ inputArea.value = localStorage.getItem("text");
 
 form.addEventListener("submit", handleSubmit);
 window.addEventListener("input", handleInput);
+
+
+let advertising = document.querySelector(".advertising");
+let choiseButton = document.querySelector(".price > .price-btn");
+
+let header = document.querySelector(".header");
+let CartNumber = document.querySelector(".counter__text");
+
+choiseButton.value = +localStorage.getItem("productCount");
+
+choiseButton.addEventListener("click", changeCart);
+console.log(+choiseButton.value);
+
+function changeCart(event) {
+    if (+choiseButton.value === 0) {
+      choiseButton.value += 1;
+      console.log(+choiseButton.value);
+      message.classList.add("message-cart");
+  
+      setTimeout(function () {
+        message.classList.remove("message-cart");
+      }, 3900);
+  
+      choiseButton.classList.add("sidebar__button_choise");
+      CartNumber.classList.add("cart-number-1");
+      choiseButton.innerHTML = "Товар уже в корзине";
+      localStorage.setItem("productCount", +choiseButton.value);
+    } else {
+      choiseButton.value -= 1;
+      console.log(+choiseButton.value);
+      choiseButton.classList.remove("sidebar__button_choise");
+      CartNumber.classList.remove("cart-number-1");
+      message.classList.remove("message-cart");
+      choiseButton.innerHTML = "Добавить в корзину";
+      localStorage.removeItem("productCount", +choiseButton.value);
+      return;
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {  
+    if (+choiseButton.value === 0) {
+      choiseButton.classList.remove("sidebar__button_choise");
+      CartNumber.classList.remove("cart-number-1");
+      choiseButton.innerHTML = "Добавить в корзину";
+    } else {
+      choiseButton.classList.add("sidebar__button_choise");
+      CartNumber.classList.add("cart-number-1");
+      choiseButton.innerHTML = "Товар уже в корзине";
+      return;
+    }
+  });
+  
